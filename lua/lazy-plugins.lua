cfdc71d66c352c46b7fab36374963f5525286dd2
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-07-10 12:43:46 +0100

    Remove tokyonight theme; Enable autopairs plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,30 +1,20 @@
 require('lazy').setup({
   require 'kickstart.plugins.guess-indent',
-
   require 'kickstart.plugins.gitsigns',
-
   require 'kickstart.plugins.which-key',
-
   require 'kickstart.plugins.telescope',
-
   require 'kickstart.plugins.lspconfig',
-
   require 'kickstart.plugins.conform',
-
   require 'kickstart.plugins.blink-cmp',
-
-  require 'kickstart.plugins.tokyonight',
-
   require 'kickstart.plugins.todo-comments',
-
   require 'kickstart.plugins.mini',
-
   require 'kickstart.plugins.treesitter',
+  require 'kickstart.plugins.autopairs',
 
+  -- require 'kickstart.plugins.tokyonight',
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
 
   { import = 'custom.plugins' },
