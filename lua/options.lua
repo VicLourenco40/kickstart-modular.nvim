3182fa6d920fc2e15a183c9ed756a0b5af6f8f6b
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-07-10 18:06:45 +0100

    Remove mouse and split options

--- lua/options.lua
+++ lua/options.lua
@@ -9,7 +9,6 @@ vim.o.breakindent = true
 vim.o.guicursor = 'a:block'
 vim.o.cursorline = true
 vim.o.scrolloff = 10
-vim.o.mouse = 'a'
 
 vim.o.signcolumn = 'yes'
 vim.o.showmode = false
@@ -22,14 +21,10 @@ end)
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
 vim.o.inccommand = 'split'
 
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
-
-vim.o.splitright = true
-vim.o.splitbelow = true
 
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
